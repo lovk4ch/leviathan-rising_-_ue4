336013123f47ba2abcb99f6658f7485cf54c7fe5
lovk4ch <lovk4ch@gmail.com>
2020-06-10 13:53:04 +0400

    Initial commit

---
+++ Source/leviathan_rising/leviathan_rising.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "leviathan_rising.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, leviathan_rising, "leviathan_rising" );
